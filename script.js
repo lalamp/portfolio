7197631203f038ca6acefe7dd4552c48e483dfb3
larissa <99204156+lal4mp@users.noreply.github.com>
2026-01-20 19:30:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+/* Navegação */
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav a');
+
+const observerOptions = {
+    threshold: 0.6 
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            navLinks.forEach(link => link.classList.remove('active'));
+            
+            const id = entry.target.getAttribute('id');
+            const activeLink = document.querySelector(`nav a[href="#${id}"]`);
+            if (activeLink) {
+                activeLink.classList.add('active');
+            }
+        }
+    });
+}, observerOptions);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+
+/* Scroll Indicator */
+const scrollIndicator = document.querySelector('.scroll-indicator');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        scrollIndicator.classList.add('hide');
+    } else {
+        scrollIndicator.classList.remove('hide');
+    }
+});
+
